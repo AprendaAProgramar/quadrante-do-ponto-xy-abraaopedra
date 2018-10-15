7868f7a3234270e438c75c947914a2b6fb0c2835
abraaopedra <44162370+abraaopedra@users.noreply.github.com>
2018-10-15 12:23:59 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,14 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Coloque aqui o seu codigo
-        
+            Console.WriteLine("Introduza variável X");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduza variável Y");
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Está no primeiro quadrante");
+            }
+            else
+            { 
+                if (x < 0 && y > 0)
+                    Console.WriteLine("Está no segundo quadrante");
+            
+                else
+            
+                    if (x > 0 && y < 0)
+                        Console.WriteLine("Está no quarto quadrante");
+            
+                    else
+            
+                        if (x < 0 && y < 0)
+                            Console.WriteLine("Está no terceiro quadrante");
+            }
+
+
         }
     }
 }
